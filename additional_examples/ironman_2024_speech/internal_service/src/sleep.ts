5dbcc4c34f3e610493f54863376f0f86404e8ee6
Nathan <tedmax100@gmail.com>
2024-08-05 01:50:41 +0800

    feat(add): del

--- additional_examples/ironman_2024_speech/internal_service/src/sleep.ts
+++
@@ -1,9 +0,0 @@
-const sleep = async (timeInSeconds: number) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, timeInSeconds)
-  })
-}
-export default sleep;
-export interface Sleep {
-  sleep: (ts: number) => Promise<unknown>
-}
